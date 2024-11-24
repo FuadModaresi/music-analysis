ded03a7733d27be5045543eaea2a400f3c4a40e3
Fuad Modaresi <50583048+FuadModaresi@users.noreply.github.com>
2024-11-24 07:04:57 -0800

    added vercel to app

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="text-center space-y-8">
         <h1 className="text-4xl font-bold text-accent">Music AI Analyzer</h1>
         <p className="max-w-xl text-gray-300">
-          This app by Fuad_MD lets you upload an audio file and analyze it for musical notes or transcriptions.
+          This app lets you upload an audio file and analyze it for musical notes or transcriptions.
           Start by uploading a file, and the app will provide a list of detected notes or transcribed text.
           Here’s an example of what you might see once the analysis is complete.
         </p>

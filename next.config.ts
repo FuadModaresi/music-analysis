7518b8208bb832fe468ea37ff6d5ce3c045b41ea
Fuad Modaresi <50583048+FuadModaresi@users.noreply.github.com>
2024-11-24 00:03:36 -0800

    added my app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // basePath: "/music-analysis",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+
 };
 
-export default nextConfig;
+module.exports = nextConfig;
